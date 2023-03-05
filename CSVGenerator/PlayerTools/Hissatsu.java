4d6be8df10eb9aa4d8c7907928f6be5ca6ec49de
florian-bizet <sunburnstar84@gmail.com>
2023-03-05 21:37:01 +0100

    hissatsu files

---
+++ CSVGenerator/PlayerTools/Hissatsu.java
@@ -0,0 +1,9 @@
+package PlayerTools;
+
+public class Hissatsu {
+    private String japaneseName;
+    private String japaneseDesc;
+
+    private int tpCost;
+    private int power; /* Not sure if it exists */
+}
