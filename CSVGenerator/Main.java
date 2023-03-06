1e9aa2bb8a29c7d33e53a625ec1f8e6747f2cebf
Florian <sunburnstar84@gmail.com>
2023-03-06 18:34:32 +0100

    Revert "Export playerList to SQL Insert Query"
    This reverts commit e0a92da278a90b90d08c6150a73b7b4dcff4b9a1.

--- CSVGenerator/Main.java
+++ CSVGenerator/Main.java
@@ -6,12 +6,10 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
-import CSVTools.CSVReader;
-import CSVTools.CSVWriter;
+import CSVExport.CSVWriter;
 import KanaTools.*;
 import PlayerTools.IEFileReader;
 import PlayerTools.Player;
-import SQLTools.SQLWriter;
 
 public class Main 
 {
@@ -23,7 +21,7 @@ public class Main
         this.kanaConverter = new KanaToRomaji();
     }
 
-    public void sourceToCSV() {
+    public void process() {
         IEFileReader.loadPlayers     (this.playerList);
         IEFileReader.loadDescriptions(this.playerList);
         IEFileReader.loadStats       (this.playerList);
@@ -33,35 +31,7 @@ public class Main
         CSVWriter.exportCSV(playerList);
     }
 
-    public void csvToSQL() {
-        this.playerList = CSVReader.loadPlayers("players.csv");
-        SQLWriter.exportSQL(playerList);
-    }
-
     public static void main(String[] args) {
-        if (args.length != 1) {
-            System.out.println("Invalid arguments !"              );
-            System.out.println("How to use : java Main -option"   );
-            System.out.println("Option list : "                   );
-            System.out.println("\t--csv : Source Files -> CSV"    );
-            System.out.println("\t--sql : CSV -> SQL Data Records");
-            return;
-        }
-
-        if (args[0].equals("--csv")) {
-            new Main().sourceToCSV();
-            return;
-        }
-
-        if (args[0].equals("--sql")) {
-            new Main().csvToSQL();
-            return;
-        }
-
-        System.out.println("Invalid arguments !"              );
-        System.out.println("How to use : java Main --option"  );
-        System.out.println("Option list : "                   );
-        System.out.println("\t--csv : Source Files -> CSV"    );
-        System.out.println("\t--sql : CSV -> SQL Data Records");
+        new Main().process();
     }
 }
